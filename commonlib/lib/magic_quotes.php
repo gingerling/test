05b990f86ef5b95ee8042ec7eef5982ea04aae9e
mdethmers <mdethmers@f73b6cb1-ca6b-434e-8ed4-fd02a03c03f0>
2004-10-26 12:10:56 +0000

    *** empty log message ***

--- commonlib/lib/magic_quotes.php
+++ commonlib/lib/magic_quotes.php
@@ -1,6 +1,4 @@
 <?
-require_once dirname(__FILE__).'/accesscheck.php';
-
 # experiment, see whether we can correct the magic quotes centrally
 
 function addSlashesArray($array) {
@@ -13,9 +11,24 @@ function addSlashesArray($array) {
   }
   return $array;
 }
-
-#$_POST = addSlashesArray($_POST);
-#$_GET = addSlashesArray($_GET);
-#$_REQUEST = addSlashesArray($_REQUEST);
-
-?>  
+if (!ini_get("magic_quotes_gpc") || ini_get("magic_quotes_gpc") == "off") {
+  $_POST = addSlashesArray($_POST);
+  $_GET = addSlashesArray($_GET);
+  $_REQUEST = addSlashesArray($_REQUEST);
+  $_COOKIE = addSlashesArray($_COOKIE);
+}
+/*
+foreach ($_POST as $key => $val) {
+  print "POST: $key = $val<br/>";
+}
+foreach ($_GET as $key => $val) {
+  print "GET: $key = $val<br/>";
+}
+foreach ($_REQUEST as $key => $val) {
+  print "REQ: $key = $val<br/>";
+}
+foreach ($_REQUEST as $key => $val) {
+  print "COOKIE: $key = $val<br/>";
+}
+*/
+?>
