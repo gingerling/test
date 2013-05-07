473dd82473cb7e7d5b5a137f07e0053507ff408d
mdethmers <mdethmers@f73b6cb1-ca6b-434e-8ed4-fd02a03c03f0>
2013-05-07 17:32:21 +0000

    don't duplicate activate call to plugins

--- phplist/public_html/lists/admin/index.php
+++ phplist/public_html/lists/admin/index.php
@@ -114,9 +114,10 @@ if (INTERFACELIB == 2 && is_file(dirname(__FILE__).'/interfacelib.php')) {
 if (!empty($_SESSION['hasconf']) || Sql_Table_exists($tables["config"],1)) {
   $_SESSION['hasconf'] = true;
   ### Activate all plugins
-  foreach ($GLOBALS['plugins'] as $plugin) {
-    $plugin->activate();
-  }
+  /* already done in pluginlib */
+  //foreach ($GLOBALS['plugins'] as $plugin) {
+    //$plugin->activate();
+  //}
 }
 if (!empty($_GET['page']) && $_GET['page'] == 'logout') {
   foreach ($GLOBALS['plugins'] as $pluginname => $plugin) {
