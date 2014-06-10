76fbbf8de268571f20c4951fa50a90a0b91a3d3b
mdethmers <mdethmers@f73b6cb1-ca6b-434e-8ed4-fd02a03c03f0>
2014-06-10 07:51:10 +0000

    add mb_basename

--- phplist/public_html/lists/admin/lib.php
+++ phplist/public_html/lists/admin/lib.php
@@ -1591,38 +1591,3 @@ function copy_recursive($source, $dest)
 
     return copy($source, $dest);
 }
-
-##http://stackoverflow.com/questions/4451664/make-php-pathinfo-return-the-correct-filename-if-the-filename-is-utf-8
-if (!function_exists("mb_basename"))
-{
-  function mb_basename($path)
-  {
-    $separator = " qq ";
-    $path = preg_replace("/[^ ]/u", $separator."\$0".$separator, $path);
-    $base = basename($path);
-    $base = str_replace($separator, "", $base);
-    return $base;
-  }
-}
-
-if (!function_exists("mb_pathinfo"))
-{
-  function mb_pathinfo($path, $opt = "")
-  {
-    $separator = " qq ";
-    $path = preg_replace("/[^ ]/u", $separator."\$0".$separator, $path);
-    if ($opt == "") $pathinfo = pathinfo($path);
-    else $pathinfo = pathinfo($path, $opt);
-
-    if (is_array($pathinfo))
-    {
-      $pathinfo2 = $pathinfo;
-      foreach($pathinfo2 as $key => $val)
-      {
-        $pathinfo[$key] = str_replace($separator, "", $val);
-      }
-    }
-    else if (is_string($pathinfo)) $pathinfo = str_replace($separator, "", $pathinfo);
-    return $pathinfo;
-  }
-}
